f6e9e9a20b4ca4ad7603b72072daba0d0b6ea54d
tarikkurtanovic <tarik2371@gmail.com>
2023-08-17 19:53:47 +0200

    static app finished

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ export default function App() {
         <FormAddFriend />
         <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -82,6 +83,31 @@ function FormAddFriend() {
       <input type="text" />
 
       <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰Bill value</label>
+      <input type="text"></input>
+
+      <label>🙅‍♂️Your expense</label>
+      <input type="text"></input>
+
+      <label>🧑‍🤝‍🧑X's expense</label>
+      <input type="text" disabled></input>
+
+      <label>💵Who is paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
     </form>
   );
 }
